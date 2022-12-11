02d36f04da80802c12709c7d30c47015c80293f0
Roy zheng <royzheng@Roys-MacBook-Air.local>
2022-12-10 22:37:25 -0700

    log in

---
+++ login/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.home, name = "home"),
+    path('signup', views.signup, name = "signup"),
+    path('loginPage', views.loginPage, name = "loginPage"),
+    path('chatroom', views.chatroom, name = "chatroom"),
+    path('polls', views.polls, name = "polls"),
+    path('logOut', views.logOut, name = "logOut"),
+    
+
+]
--- login/views.py
+++ login/views.py
@@ -1,3 +1,65 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+from .forms import CreateUserForm
+
+from django.contrib.auth.decorators import login_required
+
+
+def home(request):
+    return render (request, "login/mainpage.html")
+
+def signup(request):
+    form = CreateUserForm()
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'user succesfully created')
+            return redirect('loginPage')
+            
+    context = {'form':form}
+    return render (request, "login/signUp.html", context)
+
+def loginPage(request):
+    if request.method == "POST":
+       username = request.POST.get('username')
+       password = request.POST.get('password')
+
+       user = authenticate(request, username = username, password = password)
+
+       if user is not None:
+            login(request, user)
+            return redirect('logOut')
+
+       else:
+        messages.info(request, 'Please enter the correct Username or password')
+
+    return render (request, "login/loginPage.html")
+
+def polls(request):
+    return render (request, "login/polls.html")
+
+def chatroom(request):
+    return render (request, "login/chatroom.html")
+
+def logOut(request):
+
+    return render (request, "login/logout.html")
+
+    logout(request)
+    return redirect('loginPage')
+
+def room(request, room_name):
+	return render(request, 'chatroom.html', {
+		'room_name': room_name
+	})
+
+
+
+    
